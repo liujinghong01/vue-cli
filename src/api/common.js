871ef997559023a292649fb46298c3017da66a18
mac <mac@ljhdeMacBook-Pro.local>
2018-04-29 21:00:06 +0800

    屏幕自适应

--- src/api/common.js
+++ src/api/common.js
@@ -1,9 +1,7 @@
 
 export default {
   autoRem (){
-    var pixclPatio = 1 / window.devicePixelRatio;
-    document.write('<meta name="viewport" content="width=device-width,initial-scale='
-      +pixclPatio+',minimum-scale='+pixclPatio+',maximum-scale='+pixclPatio+',user-scalable=yes" />');
+
     var html = document.getElementsByTagName('html')[0];
     var pageWidth = html.getBoundingClientRect().width;
     html.style.fontSize = pageWidth / 15 + 'px';
