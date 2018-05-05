f9bb1a583011b29c8657c19ee3110dc22c2bf116
apple <apple@iMac.local>
2018-05-05 15:03:57 +0800

    公共方法修改

--- src/api/common.js
+++ src/api/common.js
@@ -1,10 +1,4 @@
 
 export default {
-  autoRem (){
 
-    var html = document.getElementsByTagName('html')[0];
-    var pageWidth = html.getBoundingClientRect().width;
-    html.style.fontSize = pageWidth / 15 + 'px';
-    alert(pageWidth / 15 + 'px')
-  }
 }
